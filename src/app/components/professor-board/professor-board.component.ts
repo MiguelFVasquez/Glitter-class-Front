e04672dfdda7dff95047b7c3804d963f80a68e70
miguel <florezvasquezjuanmiguel@gmail.com>
2025-04-15 12:00:06 -0500

    First commit

---
+++ src/app/components/professor-board/professor-board.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-professor-board',
+  imports: [],
+  templateUrl: './professor-board.component.html',
+  styleUrl: './professor-board.component.css'
+})
+export class ProfessorBoardComponent {
+
+}
---
+++ src/app/components/professor-header/professor-header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-professor-header',
+  imports: [],
+  templateUrl: './professor-header.component.html',
+  styleUrl: './professor-header.component.css'
+})
+export class ProfessorHeaderComponent {
+
+}
